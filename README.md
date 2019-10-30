614a76e80f58945a1f332ff826a45ed468d5661f
Maiquel Melo <maiquelml@hotmail.com>
2019-10-30 15:20:48 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Deep Learning Examples
+
+Examples and studies of Artificial Neural Network.
+
+## Notes
+Used:
+* Python 3.7
+* Anaconda 2019.10
+
+## References
+* Deep Learning Book (http://deeplearningbook.com.br)
+* Udemy Curse (https://www.udemy.com/course/machine-learning-e-data-science-com-python)
---
+++ perceptron.py
@@ -0,0 +1,63 @@
+import random
+
+class Perceptron:
+
+	def __init__(self, inputs, outputs, learning_rate=0.05, epochs=1000):
+		self.inputs = inputs
+		self.outputs = outputs
+		self.learning_rate = learning_rate
+		self.epochs = epochs
+		self.n_inputs = len(inputs)
+		self.n_inputs_values = len(inputs[0])
+		self.weights = [random.uniform(-1, 1) for i in range(self.n_inputs_values)]
+
+		self.__bias = -1
+
+	def step(self, u):
+		if u >= 0:
+			return 1
+		return 0
+
+	def train(self):
+  
+		# Configure the threshold and w0
+		for input in self.inputs:
+  			input.insert(0, self.__bias)
+		self.weights.insert(0, random.uniform(-1, 0))
+		self.n_inputs_values += 1
+
+		for e in range(self.epochs):
+				error = False
+
+				for i in range(self.n_inputs):
+					u = 0
+					for j in range(self.n_inputs_values):
+						u += self.inputs[i][j] * self.weights[j]
+					y = self.step(u)
+					
+					if y != self.outputs[i]:
+						delta_error = self.outputs[i] - y
+						for j in range(self.n_inputs_values):
+								self.weights[j] = self.weights[j] + self.learning_rate * delta_error * self.inputs[i][j]
+						error = True
+				
+				if not error:
+						print('Found epoch :', e)
+						break
+
+	def test(self, inputs):
+		inputs.insert(0, self.__bias)
+		u = 0
+		for i in range(self.n_inputs_values):
+			u += self.weights[i] * inputs[i]
+		y = self.step(u)
+		return y
+
+inputs = [[0, 0], [0, 1], [1, 0], [1, 1]]
+outputs = [0, 1, 1, 1]
+
+n = Perceptron(inputs, outputs)
+n.train()
+
+x = [0,0]
+print('Class:', n.test(x))
